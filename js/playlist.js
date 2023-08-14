eecb9a459daa8a16b4e452522a64da6766d87f41
vishalsatpute20 <136878175+vishalsatpute20@users.noreply.github.com>
2023-08-14 08:18:09 +0530

    updated

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+      name: "Close My Eyes",
+      artist: "Parker Jack",
+      img: "img1",
+      audio: "music1"
+    },
+    {
+      name: "Dandelions",
+      artist: "Ruth B",
+      img: "img2",
+      audio: "music2"
+    },
+    {
+      name: "When I'm Gone",
+      artist: "Eminem",
+      img: "img3",
+      audio: "music3"
+    },
+    {
+      name: "You And I",
+      artist: "One Direction",
+      img: "img4",
+      audio: "music4"
+    }
+  ]
